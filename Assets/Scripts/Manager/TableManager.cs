fa6be3eb268d2c76dc4c8f6988cb3eee721a3b46
TAEKGYUN-GWON <59135818+TAEKGYUN-GWON@users.noreply.github.com>
2021-03-08 20:39:38 +0900

    프로젝트 업로드

---
+++ Assets/Scripts/Manager/TableManager.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+using UnityEngine;
+
+public class TableManager
+{
+    private static TableManager _instance = null;
+
+    public static TableManager Instance
+    {
+        get
+        {
+            if(null == _instance)
+            {
+                _instance = new TableManager();
+
+                _instance.Init();
+            }
+
+            return _instance;
+        }
+    }
+
+    void Init()
+    {
+    }
+
+    private Dictionary<string, List<Dictionary<string, object>>> _table = new Dictionary<string, List<Dictionary<string, object>>>();
+
+    static string _splitRe = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
+    static string _lineSplitRe = @"\r\n|\n\r|\n|\r";
+    static char[] _trimChars = { '\"' };
+
+    public List<Dictionary<string, object>> GetTable(string file)
+    {
+        if (_table.ContainsKey(file))
+        {
+            return _table[file];
+        }
+        else
+        {
+            return Read(file);
+        }
+    }
+
+    public List<Dictionary<string, object>> Read(string file)
+    {
+        if(_table.ContainsKey(file))
+        {
+            return _table[file];
+        }
+
+        var list = new List<Dictionary<string, object>>();
+        //TextAsset data = Resources.Load (file) as TextAsset;
+
+#if (UNITY_EDITOR || UNITY_STANDALONE)
+
+        // 경로에 파일 존재하는지 확인
+        string dir = Application.dataPath + "/Data/" + file + ".csv";
+        if (!File.Exists(dir))
+        {
+            Debug.Log(file + "파일이 존재하지않습니다.");
+            return list;
+        }
+
+        string source;
+        StreamReader sr = new StreamReader(Application.dataPath + "/Data/" + file + ".csv");
+        source = sr.ReadToEnd();
+        sr.Close();
+        var lines = Regex.Split(source, _lineSplitRe);
+#else
+        TextAsset data = Resources.Load (file) as TextAsset;
+        var lines = Regex.Split(data.text, LINE_SPLIT_RE);
+#endif
+
+        if (lines.Length <= 1) return list;
+
+        var header = Regex.Split(lines[0], _splitRe);
+        for (var i = 1; i < lines.Length; i++)
+        {
+
+            var values = Regex.Split(lines[i], _splitRe);
+            if (values.Length == 0 || values[0] == "") continue;
+
+            var entry = new Dictionary<string, object>();
+            for (var j = 0; j < header.Length && j < values.Length; j++)
+            {
+                string value = values[j];
+                value = value.TrimStart(_trimChars).TrimEnd(_trimChars).Replace("\\", "");
+                object finalvalue = value;
+                int n;
+                float f;
+                if (int.TryParse(value, out n))
+                {
+                    finalvalue = n;
+                }
+                else if (float.TryParse(value, out f))
+                {
+                    finalvalue = f;
+                }
+                entry[header[j]] = finalvalue;
+            }
+            list.Add(entry);
+        }
+
+        _table.Add(file, list);
+        return list;
+    }
+}
---
+++ Assets/Scripts/ObjectBase/ObjectBase.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ObjectBase : MonoBehaviour
+{
+    protected bool _isActivated = false;
+    protected ObjectBasePool InstancePool;
+    protected Coroutine DelayDespawn = null;
+
+    public virtual bool IsActivated
+    {
+        get
+        {
+            return _isActivated;
+        }
+    }
+
+    public virtual bool IsDeactivated
+    {
+        get
+        {
+            return _isActivated == false;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+
+    }
+
+    public virtual void OnSpawn(ObjectBasePool instancePool)
+    {
+        InstancePool = instancePool;
+        _isActivated = true;
+    }
+
+    public virtual void Despawn(float delaySecond)
+    {
+        if (DelayDespawn != null)
+        {
+            StopCoroutine(DelayDespawn);
+            DelayDespawn = null;
+        }
+
+        DelayDespawn = StartCoroutine(_DelayDespawn(delaySecond));
+    }
+
+    protected virtual IEnumerator _DelayDespawn(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (IsDeactivated)
+        {
+            yield break;
+        }
+
+        DelayDespawn = null;
+        Despawn();
+    }
+
+    public virtual void Despawn()
+    {
+        if (IsDeactivated)
+        {
+            return;
+        }
+
+
+        if (DelayDespawn != null)
+        {
+            StopCoroutine(DelayDespawn);
+            DelayDespawn = null;
+        }
+
+        if (InstancePool != null)
+        {
+            InstancePool.Despawn(this);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public virtual void OnDespawn()
+    {
+        _isActivated = false;
+    }
+}
