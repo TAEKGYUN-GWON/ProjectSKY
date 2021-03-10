0d049353d46d753e4307b720ab7a4a60f3fff530
TAEKGYUN-GWON <59135818+TAEKGYUN-GWON@users.noreply.github.com>
2021-03-10 23:38:41 +0900

    -기본적인 플레이어 움직임, 카메라 이동 기능 추가

---
+++ Assets/Scripts/Camera/MainCamera.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+	public float smoothTimeX, smoothTimeY;
+
+	public float cameraGapX, cameraGapY = 1.3f;
+
+	public Vector2 velocity;
+
+	public GameObject player;
+
+	public Vector2 minPos, maxPos;
+
+	public bool bound;
+
+    private void Awake()
+    {
+		player = GameObject.FindGameObjectWithTag("Player");
+
+	}
+    void FixedUpdate()
+	{
+
+		float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x + cameraGapX, ref velocity.x, smoothTimeX);
+
+		// Mathf.SmoothDamp는 천천히 값을 증가시키는 메서드이다.
+
+		float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y + cameraGapY, ref velocity.y, smoothTimeY);
+
+		// 카메로 이동
+
+		transform.position = new Vector3(posX, posY, transform.position.z);
+
+
+
+		if (bound)
+		{
+
+			//Mathf.Clamp(현재값, 최대값, 최소값);  현재값이 최대값까지만 반환해주고 최소값보다 작으면 그 최소값까지만 반환합니다.
+
+			transform.position = new Vector3(Mathf.Clamp(transform.position.x, minPos.x, maxPos.x),
+
+				Mathf.Clamp(transform.position.y, minPos.y, maxPos.y),
+
+				Mathf.Clamp(transform.position.z, transform.position.z, transform.position.z)
+
+			);
+
+		}
+
+	}
+
+}
