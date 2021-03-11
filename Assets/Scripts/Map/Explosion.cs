309dd3187e82e29f03f20d6003bbed8d588b21bb
dolppe <35285591+dolppe@users.noreply.github.com>
2021-03-11 22:13:29 +0900

    폭팔 절반쯤 구현

---
+++ Assets/Scripts/Map/Explosion.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using UnityEngine;
+
+public static class Rigidbody2DExt
+{
+
+    public static void AddExplosionForce(this Rigidbody2D rb, float explosionForce, Vector2 explosionPosition, float explosionRadius, float upwardsModifier = 0.0F, ForceMode2D mode = ForceMode2D.Force)
+    {
+        var explosionDir = rb.position - explosionPosition;
+        var explosionDistance = explosionDir.magnitude;
+
+        // Normalize without computing magnitude again
+        if (upwardsModifier == 0)
+            explosionDir /= explosionDistance;
+        else
+        {
+            // From Rigidbody.AddExplosionForce doc:
+            // If you pass a non-zero value for the upwardsModifier parameter, the direction
+            // will be modified by subtracting that value from the Y component of the centre point.
+            explosionDir.y += upwardsModifier;
+            explosionDir.Normalize();
+        }
+
+        rb.AddForce(Mathf.Lerp(0, explosionForce, (1 - explosionDistance)) * explosionDir, mode);
+    }
+}
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField]
+    GameObject exPrefab = null;
+    [SerializeField]
+    float exForce = 0f;
+    [SerializeField]
+    Vector3 exOffset = Vector3.zero;
+
+
+
+    public void Ex()
+    {
+        GameObject exClone = Instantiate(exPrefab, transform.position, Quaternion.identity);
+        Rigidbody2D[] exRigids = exClone.GetComponentsInChildren<Rigidbody2D>();
+        for(int i =0;i<exRigids.Length;i++)
+        {
+            exRigids[i].AddExplosionForce(exForce, transform.position + exOffset, 1f);
+        }
+        gameObject.SetActive(false);
+
+
+    }
+
+
+
+
+}
--- Assets/Scripts/Map/ObjectColider.cs
+++ Assets/Scripts/Map/ObjectColider.cs
@@ -9,12 +9,32 @@ public class ObjectColider : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
+        //Destroy(gameObject);
+       // Debug.Log("asdf");
+       // if(tag == "Player")
+            
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
 
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("충돌");
+            GetComponent<Explosion>().Ex();
+            
+        }
     }
 
 
 
+    //public float explosionStrength = 100;
+    //void OnCollisionEnter2D(Collision2D _other)
+    //{
+    //    if(tag == "Player")
+    //    GetComponent<Rigidbody2D>().AddExplosionForce(explosionStrength, this.transform.position, 5);
+
+    //}
 
 
 }
