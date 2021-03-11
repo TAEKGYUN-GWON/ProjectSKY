6298a658aa311b8db4d9742b388655ca262b28d5
TAEKGYUN-GWON <59135818+TAEKGYUN-GWON@users.noreply.github.com>
2021-03-11 22:12:47 +0900

    -대쉬 기능 추가

--- Assets/Scripts/Player/Movement2D.cs
+++ Assets/Scripts/Player/Movement2D.cs
@@ -7,10 +7,28 @@ using UnityEngine;
 public class Movement2D : MonoBehaviour
 { 
     [SerializeField]
-    private float moveSpeed = 3.0f;
+    private float moveSpeed = 6.0f;
+
+    //dash
     [SerializeField]
     private float dashSpeed = 3.0f;
+    [SerializeField]
+    private float dashTime = 0f;
+    [SerializeField]
+    private float startDashTime = 0f;
+    private bool isTryDash = false;
+    private float dashDirection = 0;
 
+    [SerializeField]
+    private int maxDashCount = 0;
+    private int dashCount = 0;
+
+    [SerializeField]
+    private float dashCooldown = 0;
+    private float dashCooldownTimer = 0;
+    bool isDashCooldown = false;
+
+    float originGravityScale;
 
     [SerializeField]
     private float jumpForce = 5.0f;
@@ -19,6 +37,8 @@ public class Movement2D : MonoBehaviour
 
     [SerializeField]
     private LayerMask groundLayer;
+    [SerializeField]
+    private LayerMask platformLayer;
     private BoxCollider2D boxCollider;
     private bool isGrounded;
     private Vector3 footPosition;
@@ -28,6 +48,9 @@ public class Movement2D : MonoBehaviour
     {
         rigidbody = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+
+        dashTime = startDashTime;
+        originGravityScale = rigidbody.gravityScale;
     }
 
     public void FixedUpdate()
@@ -38,6 +61,13 @@ public class Movement2D : MonoBehaviour
 
         isGrounded = Physics2D.OverlapCircle(footPosition, 0.1f, groundLayer);
 
+        if(!isGrounded)
+        {
+            isGrounded = Physics2D.OverlapCircle(footPosition, 0.1f, platformLayer);
+        }
+
+        CheckDash();
+
     }
 
     //private void OnDrawGizmos()
@@ -47,9 +77,9 @@ public class Movement2D : MonoBehaviour
 
     //}
 
-    public void Move(float x)
+    public void Move(float _x)
     {
-        rigidbody.velocity = new Vector2(x * moveSpeed, rigidbody.velocity.y);
+        rigidbody.velocity = new Vector2(_x * moveSpeed, rigidbody.velocity.y);
     }
 
     public void Jump()
@@ -61,4 +91,70 @@ public class Movement2D : MonoBehaviour
 
     }
 
+    private void CheckDash()
+    {
+        if (dashCooldownTimer <= 0)
+        {
+            dashCount = 0;
+            dashCooldownTimer = 0f;
+            rigidbody.gravityScale = originGravityScale;
+        }
+        else
+        {
+            dashCooldownTimer -= Time.deltaTime;
+        }
+
+        if (isDashCooldown)
+        {
+            if (dashCooldownTimer <= 0)
+            {
+                isDashCooldown = false;
+            }
+        }
+        else
+        {
+            if (isTryDash)
+            {
+                if (dashTime <= 0)
+                {
+                    dashTime = startDashTime;
+                    rigidbody.velocity = Vector2.zero;
+                    isTryDash = false;
+                    rigidbody.gravityScale = originGravityScale;
+                }
+                else
+                {
+                    dashTime -= Time.deltaTime;
+                    Dash();
+                }
+            }
+        }
+
+    }
+
+    public void TryDash(float _dir)
+    {
+        if (isDashCooldown)
+            return;
+
+        if (dashCount >= maxDashCount)
+        {
+            dashCount = maxDashCount;
+            dashCooldownTimer = dashCooldown;
+            isDashCooldown = true;
+            return;
+        }
+
+        isTryDash = true;
+        dashDirection = _dir;
+
+        dashCount++;
+        dashCooldownTimer = dashCooldown;
+    }
+
+    private void Dash()
+    {
+        rigidbody.velocity = new Vector2(dashDirection * dashSpeed, rigidbody.velocity.y);
+        rigidbody.gravityScale = 0f;
+    }
 }
