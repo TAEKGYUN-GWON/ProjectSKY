fab2957408b7ef628c9d0bd2925761d2ede4b169
dolppe <35285591+dolppe@users.noreply.github.com>
2021-03-16 20:07:34 +0900

    몬스터의 공격 구현

---
+++ Assets/Scripts/Enemy/EnemyAnimationEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimationEvent : MonoBehaviour, AnimationEvent
+{
+    EnemyBattle enemyBattle;
+    private void Awake()
+    {
+        enemyBattle = GetComponentInChildren<EnemyBattle>();
+    }
+
+    public void Attack()
+    {
+        enemyBattle.Attak();
+    }
+    public void AttackEnd()
+    {
+        enemyBattle.AttackDamage();
+
+    }
+
+
+
+
+}
--- Assets/Scripts/Enemy/EnemyBattle.cs
+++ Assets/Scripts/Enemy/EnemyBattle.cs
@@ -4,6 +4,74 @@ using UnityEngine;
 
 public class EnemyBattle : MonoBehaviour
 {
+    public Animator animator;
+    public EnemyState enemyState;
+    public CapsuleCollider2D capsuleCollider2D;
+    bool bIsAttacked;
+    bool bPlayerIn;
+    bool bA;
+    float fLastAttTime;
+
+    private void Awake()
+    {
+        capsuleCollider2D = GetComponent<CapsuleCollider2D>();
+        enemyState = GetComponentInParent<EnemyState>();
+        animator = GetComponentInParent<Animator>();
+        capsuleCollider2D.enabled = true;
+        bIsAttacked = false;
+        bPlayerIn = false;
+        bA = false;
+        fLastAttTime = 0f;
+    }
+
+
+    public void Attak()
+    {
+        capsuleCollider2D.enabled = true;
+        
+}
+
+    public void AttackDamage()
+    {
+        if (bPlayerIn == true)
+            bIsAttacked = true;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (bPlayerIn == false)
+            bPlayerIn = true;
+
+
+
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+        if (bPlayerIn == false)
+            bPlayerIn = true;
+        if (collision.tag == "Player"&& Time.time>= fLastAttTime +2 )
+        {
+            animator.SetTrigger("Attack");
+            fLastAttTime = Time.time;
+
+        }
+        if (bIsAttacked)
+        {
+            //collision.GetComponent<LivingEntity>().OnDamage(enemyState.fAttDamage);
+            Debug.Log("플레이어때림");
+            bIsAttacked = false;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (bPlayerIn == true)
+            bPlayerIn = false;
+
+    }
 
 
 
