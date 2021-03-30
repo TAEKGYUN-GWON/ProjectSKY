e644ee4fbe91dde09167f8eb52941c6b964900f9
TAEKGYUN-GWON <59135818+TAEKGYUN-GWON@users.noreply.github.com>
2021-03-30 23:40:27 +0900

    인벤토리 싱글톤으로 변경 및 코드 추가

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -1,9 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInventory : MonoBehaviour
+public class PlayerInventory : Singleton<PlayerInventory>
 {
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    [SerializeField]
+    int nRelicSpace = 10;
+
+    public int RelicSpace => nRelicSpace;
+
     [SerializeField]
     List<ItemInfo> listItems = new List<ItemInfo>();
 
@@ -16,9 +24,30 @@ public class PlayerInventory : MonoBehaviour
     [SerializeField]
     SPUM_SpriteList spumSpriteList;
 
-    
+    public bool AddItem(ItemInfo item)
+    {
+        if(listItems.Count >= nRelicSpace)
+        {
+            Debug.Log("NOt enough room.");
+            return false;
+        }
+
+        listItems.Add(item);
+
+        if(onItemChangedCallback != null)
+            onItemChangedCallback.Invoke();
 
 
+        return true;
+    }
+
+    public void RemoveItem(ItemInfo item)
+    {
+        listItems.Remove(item);
+
+        if (onItemChangedCallback != null)
+            onItemChangedCallback.Invoke();
+    }
 
 
 }
