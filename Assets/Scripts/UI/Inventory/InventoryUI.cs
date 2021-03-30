480485eb8d84543672f86e95a648f4766d012ebd
TAEKGYUN-GWON <59135818+TAEKGYUN-GWON@users.noreply.github.com>
2021-03-30 23:40:53 +0900

    -인벤토리 UI 추가 및 scripts/UI 폴더 생성

---
+++ Assets/Scripts/UI/Inventory/InventoryUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    PlayerInventory inventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = PlayerInventory.Instance;
+        inventory.onItemChangedCallback += UpdateUI;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateUI()
+    {
+        Debug.Log("UPDATING UI");
+    }
+}
